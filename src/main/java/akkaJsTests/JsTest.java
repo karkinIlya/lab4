a5474be3c02635df342166fdc700f1dc4fe8cb22
Ilya Karkin <karkinia@yandex.ru>
2020-11-13 20:28:28 +0300

    Scripted auto-commit on change (2020-11-13 20:28:28) by gitwatch.sh

--- src/main/java/akkaJsTests/JsTest.java
+++ src/main/java/akkaJsTests/JsTest.java
@@ -1,4 +1,27 @@
 package akkaJsTests;
 
-public class JsTest {
+import java.util.List;
+
+public class JsTest <ResultType, ParamsType> {
+    private String testName;
+    private ResultType expectedResult;
+    private List<ParamsType> params;
+
+    public JsTest(String testName, ResultType expectedResult, List<ParamsType> params) {
+        this.testName = testName;
+        this.expectedResult = expectedResult;
+        this.params = params;
+    }
+
+    public String getTestName() {
+        return testName;
+    }
+
+    public ResultType getExpectedResult() {
+        return expectedResult;
+    }
+
+    public List<ParamsType> getParams() {
+        return params;
+    }
 }
--- src/main/java/akkaJsTests/StoreActor.java
+++ src/main/java/akkaJsTests/StoreActor.java
@@ -1,7 +1,17 @@
 package akkaJsTests;
 
 import akka.actor.AbstractActor;
+import akka.actor.UntypedActor;
 
-public class StoreActor extends AbstractActor {
-    private Map<String, >
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class StoreActor extends UntypedActor {
+    private Map<String, JsTest<String, Integer>> store = new HashMap<>();
+
+    @Override
+    public void onReceive(Object message) throws Throwable {
+
+    }
 }
