1a3d23af452120ff66a9fc9c2a0289e8c331c25c
Ilya Karkin <karkinia@yandex.ru>
2020-11-21 15:50:50 +0300

    Scripted auto-commit on change (2020-11-21 15:50:50) by gitwatch.sh

--- src/main/java/akkaJsTests/AkkaApp.java
+++ src/main/java/akkaJsTests/AkkaApp.java
@@ -24,8 +24,6 @@ public class AkkaApp extends AllDirectives {
         final ActorMaterializer materializer = ActorMaterializer.create(system);
 
         AkkaApp app = new AkkaApp();
-
-        
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = app.crea
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(routeFlow,
                 ConnectHttp.toHost("localhost", 8080), materializer);
